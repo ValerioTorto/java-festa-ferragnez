12818d68cf24b7db35a5103441e66e9546f7331d
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-30 16:48:42 +0100

    end

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,6 @@
 package com.ferragnez.party;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CheckGuest {
@@ -9,32 +10,35 @@ public class CheckGuest {
 
         //CREO ARRAY DEGLI INVITATI
         String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-
+        System.out.println(Arrays.toString(guests));
         //CHIEDO IL NOME ALL'INVITATO
         System.out.println("Dimmi il tuo nome: ");
         String name = scanner.nextLine();
-        boolean equals = false;
+        boolean found = false;
+
         for (int i = 0; i < guests.length; i++) {
 
 
 
 
             if (name.equals(guests[i])) {
+                found = true;
 
-                System.out.println("Prego puoi entrare");
-                equals = true;
-                break;
             } else {
-                System.out.println("Ora chiamo la polizia");
-
-
 
             }
+
             scanner.close();
         }
+        if (found == true) {
+            System.out.println("Prego " + name + ", entri pure");
+        } //else if(name == .guests[0]) {
+            //System.out.println("Dua Lipa tornatene in Albania");
+        //}
+        else {
+            System.out.println("Adesso chiamo la polizia");
+        }
     }
-
-
 }
 
 
